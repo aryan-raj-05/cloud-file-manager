cf17c4d08f3469b3eb05faf12f4f935afea27ab2
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-08-03 23:42:44 +0530

    intialized: frontend with some other libraries

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-blue-500 text-white flex justify-between p-4 pl-6 pr-6">
+      <div>Hello World!</div>
+      <Button onClick={() => setCount(count + 1)}>Click Here</Button>
+      <div>{count}</div>
+    </div>
+  );
+}
+
+export default App;
