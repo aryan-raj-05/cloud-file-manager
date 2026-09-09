0f2037fda0d92f10c9715e1f36bb69311fba9d68
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-09-09 09:32:52 +0530

    removed frontend and workspaces

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,5 +3,5 @@ import "dotenv/config";
 export const config = {
   PORT: Number(process.env.PORT) || 3001,
   DATABASE_URL: process.env.DATABASE_URL!,
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV: process.env.NODE_ENV!,
 };
